85da4e22452bf711ec629b98f2b5a6631a20c69f
Andy <1013651@student.sae.edu.au>
2024-03-11 13:53:41 +1000

    Imported old assets for reference

---
+++ Assets/Old Flocking Assets/Scripts/Character.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Character : Minion {
+	protected List<GameObject> markerPoints;
+	protected int currentMarker = -1;
+
+	protected Rigidbody rb;
+
+	public float MarkerReachedThreshold = 0.1f;
+
+	public float Speed = 5f;
+
+	public float MinSpeedPercentage = 0.1f;
+	public float MaxSpeedPercentage = 1f;
+	public float MinSpeedDistance = 0.5f;
+	public float MaxSpeedDistance = 2f;
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		base.Start();
+
+		// find all of the marker points
+		markerPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("Marker"));	
+
+		// retrieve the rigid body
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () 
+	{
+		Update_Navigation();
+	}
+
+	protected void Update_Navigation()
+	{
+		// no markers?
+		if ((markerPoints == null) || (markerPoints.Count == 0))
+			return;
+
+		// do we have a current marker? if not start at a random one
+		if (currentMarker < 0)
+			currentMarker = Random.Range(0, markerPoints.Count);
+
+		// have we reached the current marker?
+		Vector2 distanceToMarker = new Vector2(markerPoints[currentMarker].transform.position.x - transform.position.x,
+											   markerPoints[currentMarker].transform.position.z - transform.position.z);
+		if (distanceToMarker.magnitude < MarkerReachedThreshold)
+		{
+			// pick a new marker - could pick the same one but we'll take the chance
+			currentMarker = Random.Range(0, markerPoints.Count);
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		Update_Steering();
+	}
+
+	protected void Update_Steering()
+	{
+		// no markers? or current point?
+		if ((markerPoints == null) || (markerPoints.Count == 0) || (currentMarker < 0))
+			return;
+
+		// calculate the vector to the marker
+		Vector2 vectorToMarker = new Vector2(markerPoints[currentMarker].transform.position.x - transform.position.x,
+										     markerPoints[currentMarker].transform.position.z - transform.position.z);
+		
+		// store distance to marker
+		float distToMarker = vectorToMarker.magnitude;
+
+		// calculate the desired velocity
+		Vector3 desiredVelocity = new Vector3(vectorToMarker.x, 0, vectorToMarker.y);
+		desiredVelocity.Normalize();
+
+		Debug.DrawLine(transform.position, markerPoints[currentMarker].transform.position, Color.white);
+		Debug.DrawRay(transform.position + Vector3.up, desiredVelocity.normalized*2f, Color.green);
+
+		// slew the character's rotation around into the direction of movement
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(desiredVelocity, Vector3.up), 60f * Time.deltaTime);
+
+		// lerp the speed between our ranges
+		float distancePercentage = Mathf.Clamp01((distToMarker - MinSpeedDistance) / (MaxSpeedDistance - MinSpeedDistance));
+		float speedScale = Mathf.Lerp(MinSpeedPercentage, MaxSpeedPercentage, distancePercentage);
+
+		// apply the desired velocity
+		rb.velocity = transform.forward * Speed * speedScale;
+		velocity = rb.velocity;
+	}
+}
---
+++ Assets/Old Flocking Assets/Scripts/Minion.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Minion : MonoBehaviour 
+{
+	protected Vector3 velocity = Vector3.zero;
+	protected List<Minion> minions;
+
+	protected MinionConfig Config;
+	
+	public virtual Vector3 Velocity 
+	{
+		get 
+		{
+			return velocity;
+		}
+		set 
+		{
+			velocity = value;
+		}
+	}
+
+	// Use this for initialization
+	protected virtual void Start () 
+	{
+		// retreive all minions
+		minions = new List<Minion>(FindObjectsOfType<Minion>());	
+
+		// retrieve the minion config
+		Config = FindObjectOfType<MinionConfig>();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () 
+	{
+		// Calculate and apply the flocking vector
+		velocity = (velocity + Flocking_Update()).normalized * Config.Speed;
+
+		// apply the velocity
+		transform.position += velocity * Time.deltaTime;
+	}
+
+	protected Vector3 Flocking_Update() 
+	{
+		// Calculate each of the vectors
+		Vector3 cohesionVector = Flocking_CalculateCohesion(minions) * Config.CohesionStrength;
+		Vector3 separationVector = Flocking_CalculateSeparation(minions) * Config.SeparationStrength;
+		Vector3 alignmentVector = Flocking_CalculateAlignment(minions) * Config.AlignmentStrength;
+
+		Debug.DrawLine(transform.position + Vector3.up, transform.position + Vector3.up + cohesionVector, Color.red);
+		Debug.DrawLine(transform.position + Vector3.up, transform.position + Vector3.up + separationVector, Color.green);
+		Debug.DrawLine(transform.position + Vector3.up, transform.position + Vector3.up + alignmentVector, Color.blue);
+
+		// Calculate the flocking vector
+		Vector3 flockingVector = (cohesionVector + separationVector + alignmentVector) * Config.Speed;
+
+		return flockingVector;
+	}
+
+	Vector3 Flocking_CalculateCohesion(List<Minion> flock) 
+	{
+		// My location = transform.position
+		// Entire flock - flock
+
+		Vector3 cohesion = Vector3.zero;
+		Vector3 cohesionOfMinions = Vector3.zero;
+
+		// count of minion = average of minion positions
+		// cohesion = position of other minion - position of self
+
+		for (int i = 0; i < flock.Count; i++)
+		{
+			var minionVar = flock[i];
+			var minionPosition = minionVar.transform.position;
+
+			cohesionOfMinions += minionPosition;
+		}
+		cohesionOfMinions /= flock.Count;
+
+		cohesion = cohesionOfMinions - this.transform.position;
+
+		return cohesion.normalized;
+	}
+
+	Vector3 Flocking_CalculateSeparation(List<Minion> flock)
+	{
+		// My location = this.transform.position
+		// Entire flock - flock
+
+		Vector3 separation = Vector3.zero;		// The separation vector
+		Vector3 othersToSelf = Vector3.zero;	// The vector of other minions
+
+		// separation = average of other vectors to self
+
+		for (int i = 0; i < flock.Count; i++)
+		{
+			var minionVar = flock[i];
+			if (minionVar == this)
+            {
+				continue; // If the minion in the flock is this minion, skip it
+            }
+			else
+            {
+				var minionPosition = minionVar.transform.position;
+				othersToSelf += this.transform.position - minionPosition;
+			}
+		}
+
+		separation = othersToSelf / (flock.Count - 1);
+
+		return separation.normalized;
+	}
+
+	Vector3 Flocking_CalculateAlignment(List<Minion> flock) 
+	{
+		// My location = transform.position
+		// My velocity = this.Velocity
+		// Entire flock - flock
+
+		Vector3 alignment = Vector3.zero;
+
+        // alignment = average of velocity vector for each memeber
+        //			 = (add the velocity vectors of all of the flock) / number of velocity vectors
+        // number of velocity vectors we added = number of minions in flock = flock.Count
+
+        for (int i = 0; i < flock.Count; i++)
+        {
+			var minionVar = flock[i];
+			var minionVelocity = minionVar.Velocity;
+
+			alignment += minionVelocity;
+        }
+
+		alignment /= flock.Count;
+
+		return alignment.normalized;
+	}
+}
